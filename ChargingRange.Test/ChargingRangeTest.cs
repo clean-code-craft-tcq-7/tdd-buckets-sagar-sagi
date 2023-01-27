8a7ac5ce20b638d3e1e44603b4a122d5cf056942
APAC\YSF1KOR <sagar.ty@in.bosch.com>
2023-01-27 16:19:14 +0530

    Added the solution and csproj files

--- ChargingRange.Test/ChargingRangeTest.cs
+++ ChargingRange.Test/ChargingRangeTest.cs
@@ -1,19 +1,20 @@
-using System;
+﻿using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace ChargingRange.Test
 {
     public class ChargingRangeTest
     {
-    
-    [Fact]
-    public void TestSampleRange()
-    {
-        int[] sampleRange = new int[2] { 4, 5 };
-        string rangeReport = ChargingRange.GetRangeReport(sampleRange);
 
-        Assert.True(rangeReport == "4-5, 2");
-    }
-    
+        [Fact]
+        public void TestSampleRange()
+        {
+            List<int> sampleRange = new List<int>() { 4, 5 };
+            string rangeReport = ChargingRange.GetRangeReport(sampleRange);
+
+            Assert.True(rangeReport == "4-5, 2");
+        }
+
     }
 }
